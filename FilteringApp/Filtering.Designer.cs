1957f834605a57160bd11f3bceace9583751fdfa
simonsrz <szymonslusarz01@gmail.com>
2023-02-23 11:53:08 +0100

    contrast and brightness adjustments added

--- FilteringApp/Filtering.Designer.cs
+++ FilteringApp/Filtering.Designer.cs
@@ -34,12 +34,13 @@
             this.modifiedImage = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.contrastButton = new System.Windows.Forms.Button();
+            this.brightnessButton = new System.Windows.Forms.Button();
             this.inversionButton = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.loadImageMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reloadImageMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.initialImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.modifiedImage)).BeginInit();
@@ -84,8 +85,8 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.contrastButton);
+            this.groupBox1.Controls.Add(this.brightnessButton);
             this.groupBox1.Controls.Add(this.inversionButton);
             this.groupBox1.Location = new System.Drawing.Point(1246, 59);
             this.groupBox1.Name = "groupBox1";
@@ -103,23 +104,25 @@
             this.button4.Text = "Gamma correction";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // contrastButton
             // 
-            this.button3.Location = new System.Drawing.Point(6, 182);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(304, 46);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Contrast enhancement";
-            this.button3.UseVisualStyleBackColor = true;
+            this.contrastButton.Location = new System.Drawing.Point(6, 182);
+            this.contrastButton.Name = "contrastButton";
+            this.contrastButton.Size = new System.Drawing.Size(304, 46);
+            this.contrastButton.TabIndex = 2;
+            this.contrastButton.Text = "Contrast enhancement";
+            this.contrastButton.UseVisualStyleBackColor = true;
+            this.contrastButton.Click += new System.EventHandler(this.contrastButton_Click);
             // 
-            // button2
+            // brightnessButton
             // 
-            this.button2.Location = new System.Drawing.Point(6, 114);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(304, 46);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Brightness correction";
-            this.button2.UseVisualStyleBackColor = true;
+            this.brightnessButton.Location = new System.Drawing.Point(6, 114);
+            this.brightnessButton.Name = "brightnessButton";
+            this.brightnessButton.Size = new System.Drawing.Size(304, 46);
+            this.brightnessButton.TabIndex = 1;
+            this.brightnessButton.Text = "Brightness correction";
+            this.brightnessButton.UseVisualStyleBackColor = true;
+            this.brightnessButton.Click += new System.EventHandler(this.brightnessButton_Click);
             // 
             // inversionButton
             // 
@@ -144,7 +147,8 @@
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.loadImageMenuItem});
+            this.loadImageMenuItem,
+            this.reloadImageMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(1574, 40);
@@ -157,6 +161,13 @@
             this.loadImageMenuItem.Size = new System.Drawing.Size(158, 36);
             this.loadImageMenuItem.Text = "Load image";
             this.loadImageMenuItem.Click += new System.EventHandler(this.loadImage_Click);
+            // 
+            // reloadImageMenuItem
+            // 
+            this.reloadImageMenuItem.Name = "reloadImageMenuItem";
+            this.reloadImageMenuItem.Size = new System.Drawing.Size(179, 36);
+            this.reloadImageMenuItem.Text = "Reload image";
+            this.reloadImageMenuItem.Click += new System.EventHandler(this.reloadImageMenuItem_Click);
             // 
             // Filtering
             // 
@@ -194,9 +205,10 @@
         private Button inversionButton;
         private GroupBox groupBox2;
         private Button button4;
-        private Button button3;
-        private Button button2;
+        private Button contrastButton;
+        private Button brightnessButton;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem loadImageMenuItem;
+        private ToolStripMenuItem reloadImageMenuItem;
     }
 }
--- FilteringApp/Filtering.cs
+++ FilteringApp/Filtering.cs
@@ -1,8 +1,11 @@
+using System.Diagnostics.Contracts;
+
 namespace FilteringApp
 {
     public partial class Filtering : Form
     {
-        Bitmap modifiedImg;
+        Bitmap modifiedImg = null;
+        Bitmap initialImg = null;
         public Filtering()
         {
             InitializeComponent();
@@ -20,9 +23,16 @@ namespace FilteringApp
             if (imgDialog.ShowDialog() == DialogResult.OK)
             {
                 initialImage.Image = new Bitmap(imgDialog.FileName);
+                initialImg = new Bitmap(imgDialog.FileName);
                 modifiedImage.Image = new Bitmap(imgDialog.FileName);
                 modifiedImg = new Bitmap(imgDialog.FileName);
             }
+        }
+
+        private void reloadImageMenuItem_Click(object sender, EventArgs e)
+        {
+            modifiedImage.Image = initialImg;
+            modifiedImg = initialImg;
         }
 
         private void inversionButton_Click(object sender, EventArgs e)
@@ -35,8 +45,75 @@ namespace FilteringApp
                 {
                     Color currPixel = modifiedImg.GetPixel(x, y);
 
-                    currPixel = Color.FromArgb(255 - currPixel.R, 255 -
-                       currPixel.G, 255 - currPixel.B);
+                    currPixel = Color.FromArgb(255 - currPixel.R, 255 - currPixel.G, 255 - currPixel.B);
+
+                    afterModificationMap.SetPixel(x, y, currPixel);
+                }
+            }
+            modifiedImage.Image = afterModificationMap;
+            modifiedImg = afterModificationMap;
+        }
+
+        private void brightnessButton_Click(object sender, EventArgs e)
+        {
+            Bitmap afterModificationMap = new Bitmap(modifiedImg.Width, modifiedImg.Height);
+
+            for (int x = 0; x < modifiedImg.Width; x++)
+            {
+                for (int y = 0; y < modifiedImg.Height; y++)
+                {
+                    Color currPixel = modifiedImg.GetPixel(x, y);
+
+                    int red = currPixel.R + 10;
+                    int green = currPixel.G + 10;
+                    int blue = currPixel.B + 10;
+
+                    if (red > 255)
+                        red = 255;
+                    if (green > 255)
+                        green = 255;
+                    if (blue > 255)
+                        blue = 255;
+
+                    currPixel = Color.FromArgb(red, green, blue);
+
+                    afterModificationMap.SetPixel(x, y, currPixel);
+                }
+            }
+            modifiedImage.Image = afterModificationMap;
+            modifiedImg = afterModificationMap;
+        }
+
+        private void contrastButton_Click(object sender, EventArgs e)
+        {
+            double contrastLevel = Math.Pow((100.0 + 10) / 100.0, 2);
+
+            Bitmap afterModificationMap = new Bitmap(modifiedImg.Width, modifiedImg.Height);
+
+            for (int x = 0; x < modifiedImg.Width; x++)
+            {
+                for (int y = 0; y < modifiedImg.Height; y++)
+                {
+                    Color currPixel = modifiedImg.GetPixel(x, y);
+
+                    int red = (int)(((((currPixel.R / 255.0) - 0.5) * contrastLevel) + 0.5) * 255.0);
+                    int green = (int)(((((currPixel.G / 255.0) - 0.5) * contrastLevel) + 0.5) * 255.0);
+                    int blue = (int)(((((currPixel.B / 255.0) - 0.5) * contrastLevel) + 0.5) * 255.0);
+
+                    if (red > 255)
+                        red = 255;
+                    if (green > 255)
+                        green = 255;
+                    if (blue > 255)
+                        blue = 255;
+                    if (red < 0)
+                        red = 0;
+                    if (green < 0)
+                        green = 0;
+                    if (blue < 0)
+                        blue = 0;
+
+                    currPixel = Color.FromArgb(red, green, blue);
 
                     afterModificationMap.SetPixel(x, y, currPixel);
                 }
